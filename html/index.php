17534bfbd4aad42467e1803d3ea621e3ae5b1164
thangtran <thang.tranngoc@mor.com.vn>
2018-11-19 16:53:33 +0700

    part 2

--- html/index.php
+++ html/index.php
@@ -9,7 +9,81 @@
     <section class="main">
         <?php include 'views/header.php';?>
         <section class="wrapper">
-        
+        	<div class="banner-page">
+        		<div class="container-fluid">
+        			<div class="row">
+        				<div class="col-sm-12 padding-0">
+        					<div class="regular1 slider">
+        						<div class="item1">
+        							<a href="#" class="banner1">
+        								<img src="resources/images/index/banner-3.jpg" class="img-responsive" alt="">
+        								<div class="txt-banner">
+        									<h4>The coolest & most powerful</h4>
+        									<h3>Pronunciation</h3>
+        									<h5>Training</h5>
+        								</div>
+        							</a>
+        						</div>
+        						<div class="item1">
+        							<a href="#" class="banner1">
+        								<img src="resources/images/index/banner-3.jpg" class="img-responsive" alt="">
+        								<div class="txt-banner">
+        									<h4>The coolest & most powerful</h4>
+        									<h3>Pronunciation</h3>
+        									<h5>Training</h5>
+        								</div>
+        							</a>
+        						</div>
+        					</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+			<div class="option-1">
+				<div class="container border-box">
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="title-page">
+								<h2>Khóa Học</h2>
+							</div>
+						</div>
+					</div>
+
+					<div class="row mb-50">
+						<div class="col-sm-6">
+							<div class="video-intro">
+								<iframe width="100%" height="400" src="https://www.youtube.com/embed/cX2dMKYYZI4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="txt-right-op2">
+								<h2>English pronunciation training course</h2>
+								<ul class="btn-khoahoc">
+									<li><a href="#">Chi tiết</a></li>
+									<li><a href="">Tham gia học ngay</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<div class="row mb-50">
+						<div class="col-sm-6">
+							<div class="video-intro">
+								<iframe width="100%" height="400" src="https://www.youtube.com/embed/cX2dMKYYZI4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="txt-right-op2">
+								<h2>English pronunciation training course</h2>
+								<ul class="btn-khoahoc">
+									<li><a href="#">Chi tiết</a></li>
+									<li><a href="">Tham gia học ngay</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
         </section>
         <?php include 'views/footer.php';?>
     </section>
